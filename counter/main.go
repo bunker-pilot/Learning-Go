56f1c19975dd561682ea0b7687ff17a1401703a2
Erfan Dousti <erfan.flashpoint@gmail.com>
2025-10-28 22:33:28 +0330

    Made counter

--- counter/main.go
+++ counter/main.go
@@ -1,4 +1,28 @@
 package main
-func main()  {
-	
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	data , _ := os.ReadFile("./words.txt")
+	content := string(data)
+
+	i:=0
+	start :=0
+	words := []string{}
+	for i < len(content){
+		start = i
+		for{
+			if content[i] != 10 && content[i] !=32{
+				i +=1
+			} else{
+				words = append(words, content[start:i])
+				break
+			}
+		}
+		
+	}
+	fmt.Println(len(words))
 }
