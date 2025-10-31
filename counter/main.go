367a08be756cd337d74874163174bdc8c30b39a9
Erfan Dousti <erfan.flashpoint@gmail.com>
2025-10-31 23:40:07 +0330

    Added two edge cases

--- counter/main.go
+++ counter/main.go
@@ -12,10 +12,14 @@ func main() {
 
 func CountWords(data []byte) int{
 	counter :=0
+	if len(data) ==0{
+		return counter
+	}
 	for _ , i := range data{
-		if i ==' '{
+		if i == ' '{
 			counter +=1
 		}
 	}
+	counter++
 	return counter
 }
--- counter/main_test.go
+++ counter/main_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestCountwords(t *testing.T) {
-	input := "One two three "
+	input := "One two three"
 	expected := 3
 	result := CountWords([]byte(input))
 	if result != expected{
-		t.Fail()
+		t.Error("Expected:", expected, "got:", result)
+	}
+	input = ""
+	expected = 0
+	result = CountWords([]byte(input))
+	if result != expected{
+		t.Error("expected:", expected, "got:", result)
+	}
+	input = " "
+	expected = 0
+	result = CountWords([]byte(input))
+	if result != expected{
+		t.Error("expected:", expected, "got:", result)
 	}
 }
