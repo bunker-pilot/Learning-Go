d62428d5d1b58107001ae245ee8a27205cdbb1f3
Erfan Dousti <erfan.flashpoint@gmail.com>
2025-11-01 21:33:03 +0330

    Better Error Management

--- counter/main.go
+++ counter/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
-	data , _ := os.ReadFile("./words.txt")
+	data , err := os.ReadFile("./words.txt")
+	log.SetFlags(0)
+	if err != nil{
+		log.Fatalln("Failed to read file:", err)
+	}
 	fmt.Println(CountWords(data))
 }
 
